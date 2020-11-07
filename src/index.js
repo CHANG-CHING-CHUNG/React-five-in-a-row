4a19f2623c61ca31d80f46f94e146df75c89ae9a
CHANG-CHING-CHUNG <54635661+CHANG-CHING-CHUNG@users.noreply.github.com>
2020-11-07 22:46:12 +0800

    Reacr official tutorial completed

--- src/index.js
+++ src/index.js
@@ -10,24 +10,13 @@ function Square({ value, onClick }) {
   );
 }
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
-  function handkeClick(i) {
-    const newSquares = squares.slice();
-    newSquares[i] = "X";
-    setSquares(newSquares);
-  }
-
+function Board(props) {
   function renderSquare(i) {
-    return <Square value={squares[i]} onClick={() => handkeClick(i)} />;
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
   }
-
-  const status = "Next player: X";
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -48,17 +37,94 @@ function Board() {
 }
 
 function Game() {
+  const [state, setState] = useState({
+    history: [
+      {
+        squares: Array(9).fill(null),
+      },
+    ],
+    stepNumber: 0,
+    xIsNet: true,
+  });
+
+  function handkeClick(i) {
+    const history = state.history.slice(0, state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const newSquares = current.squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = state.xIsNet ? "X" : "O";
+    setState({
+      history: history.concat([
+        {
+          squares: newSquares,
+        },
+      ]),
+      stepNumber: history.length,
+      xIsNet: !state.xIsNet,
+    });
+  }
+
+  function jumpTo(step) {
+    setState({
+      history: state.history.slice(0, step + 1),
+      stepNumber: step,
+      xIsNet: step % 2 === 0,
+    });
+  }
+
+  const history = state.history;
+  const current = history[state.stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+  let status;
+
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (state.xIsNet ? "X" : "O");
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handkeClick(i)} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
